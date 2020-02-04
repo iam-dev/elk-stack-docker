4bf1071a2486036f46457ff845fa8ec3a502891a
Elky Bachtiar <elky.bachtiar@duo.nl>
2020-02-04 10:24:24 +0100

    firts commit

---
+++ scripts/setup-users.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# SET .env file
+if [ -f .env ]
+then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+fi
+
+set -euo pipefail
+
+#cacert=/config/elasticsearch/ca/ca.crt
+# Wait for ca file to exist before we continue. If the ca file doesn't exist
+# then something went wrong.
+:'while [ ! -f $cacert ]
+do
+  sleep 2
+done
+ls -l $cacert
+'
+
+#es_url=https://elastic:${ELASTIC_PASSWORD}@elasticsearch:9200
+es_url=http://elastic:${ELASTIC_PASSWORD}@${ELASTIC_USERNAME}:${ELASTIC_PORT}
+# Wait for Elasticsearch to start up before doing anything.
+#until curl -s --cacert $cacert $es_url -o /dev/null; do
+until curl -s $es_url -o /dev/null; do
+    sleep 1
+done
+
+# Set the password for the kibana user.
+# REF: https://www.elastic.co/guide/en/x-pack/6.0/setting-up-authentication.html#set-built-in-user-passwords
+#until curl --cacert $cacert -s -H 'Content-Type:application/json' \
+until curl -s -H 'Content-Type:application/json' \
+     -XPUT $es_url/_xpack/security/user/kibana/_password \
+     -d "{\"password\": \"${ELASTIC_PASSWORD}\"}"
+do
+    sleep 2
+    echo Retrying...
+done
+
+#until curl --cacert $cacert -s -H 'Content-Type:application/json' \
+until curl -s -H 'Content-Type:application/json' \
+     -XPUT $es_url/_xpack/security/user/logstash_system/_password \
+     -d "{\"password\": \"${ELASTIC_PASSWORD}\"}"
+do
+    sleep 2
+    echo Retrying...
+done
